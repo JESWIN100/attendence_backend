07b6c8aefce4e51e6c81de9d9f98eec3e8e6e29b
JESWIN100 <josepjjeswin20@gmail.com>
2025-06-17 12:00:56 +0530

    third commit

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ app.use(express.json());
 
 
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
